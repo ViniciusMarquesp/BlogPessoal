bbfc66c4156e5f2b166f24ae9dd911c65ca9d02d
ViniciusMarquesp <ciusivinicius@hotmail.com>
2021-08-31 12:30:17 -0300

    Componente tema estrutura

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
+import { TemaComponent } from './tema/tema.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'entrar', pathMatch: 'full' },
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: 'entrar', component: EntrarComponent },
   { path: 'cadastrar', component: CadastrarComponent },
   { path: 'inicio', component: InicioComponent },
+  { path: 'tema', component: TemaComponent },
 ];
 
 @NgModule({
